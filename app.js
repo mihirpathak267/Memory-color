212898d97bd3a88681e86e73101c0782cd50763f
Mihir Pathak <mihirpathak267@gmail.com>
2023-02-28 17:17:03 +0530

    Create /change route to render svg images.
    Use bootstrap and js scripts to randomly generate 2 different types of
    forms in the change ejs file.

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.post("/", function(req, res){
+    console.log(req.body.rgbColor);
+});
+const defaultArray = ["banana", "rectangle"];
+// make a get route for the change ejs file that randomly changes the stimuli shown
+app.get("/change", function(req, res){
+    var change = Math.floor(Math.random()*2);
+    var stimuli = defaultArray[change];
+    res.render("change", {stimuliTitle: stimuli});
+})
+
 app.listen(4000, function(req, res){
     console.log("Server is listening on port 4000");
 });
--- public/scripts/color.js
+++ public/scripts/color.js
@@ -1,42 +1,42 @@
+const colorPicker = document.getElementById("color-picker");
+const marker = document.getElementById("color-picker-marker");
 
-		const colorPicker = document.getElementById("color-picker");
-		const marker = document.getElementById("color-picker-marker");
+function getColor(x) {
+	let r = 255;
+	let g = 255 - (Math.floor(x / 10) * 3);
+	let b = 0;
+	return "rgb(" + r + "," + g + "," + b + ")";
+}
+function updateMarker(x) {
+	marker.style.left = x + "px";
+	marker.style.backgroundColor = getColor(x);
+	console.log(getColor(x));
+	document.getElementById("rgb").setAttribute("value", getColor(x));
+}
 
-		function getColor(x) {
-			let r = 255;
-			let g = 255 - (Math.floor(x / 10) * 3);
-			let b = 0;
-			return "rgb(" + r + "," + g + "," + b + ")";
+function getPosition(element) {
+	let xPosition = 0;
+
+	while (element) {
+		xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+		element = element.offsetParent;
 		}
 
-		function updateMarker(x) {
-			marker.style.left = x + "px";
-			marker.style.backgroundColor = getColor(x);
-			console.log(getColor(x));
-		}
+		return xPosition;
+}
 
-		function getPosition(element) {
-			let xPosition = 0;
+colorPicker.addEventListener("click", function(event) {
+	let x = event.pageX - getPosition(this);
+	updateMarker(x);
+});
 
-			while (element) {
-				xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-				element = element.offsetParent;
-			}
-
-			return xPosition;
-		}
-
-		colorPicker.addEventListener("click", function(event) {
-			let x = event.pageX - getPosition(this);
-			updateMarker(x);
-		});
-
-		updateMarker(Math.floor(Math.random() * 300));
+updateMarker(Math.floor(Math.random() * 300));
 // Get the image element
 var image = document.getElementById("my-image");
 var rectangle = document.getElementById("rectangle");
 
 // Generate a random color using RGB values
+
 var r = 255;
 var g = 159 + Math.floor(Math.random() * 33);
 var b = 0;
@@ -45,3 +45,4 @@ var color = "rgb(" + r + "," + g + "," + b + ")";
 // Set the image's background color using the random color
 image.style.fill = color;
 rectangle.style.fill = color;
+
